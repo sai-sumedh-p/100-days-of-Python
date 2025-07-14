59aa402d8275ffdcdf1ef2d64431f7b5043bb425
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-07-14 22:56:26 +0530

    initial files

---
+++ leftshift.py
@@ -0,0 +1,6 @@
+'''Given 2 numbers N & K, print the number after performing left shift of N,'K' times.
+Output displays the Kth left shift of value N.'''
+N = 5
+K = 2
+result = N << K
+print(result) 
