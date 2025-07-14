694779256155f646efc4d099f30108f27537cff9
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-07-14 22:50:23 +0530

    initial files

--- SimpleIntrest.py
+++ SimpleIntrest.py
@@ -4,3 +4,5 @@ R=float(input("enter the intrest rate"))
 T=int(input("enter the time in years"))
 SI=(P*T*R)/100
 print(f"The Simple Intrest is {SI}")
+total=P+SI
+print(f"the total payable amount is : {total}")
