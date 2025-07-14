cc5a3168676ddec93f35d5ef20a3570add9ff52d
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-07-14 21:53:21 +0530

    initial files

---
+++ SimpleIntrest.py
@@ -0,0 +1,6 @@
+
+P=float(input("enter the principle amount"))
+R=float(input("enter the intrest rate"))
+T=int(input("enter the time in years"))
+SI=(P*T*R)/100
+print(f"The Simple Intrest is {SI}")
