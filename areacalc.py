6c678c0c3f25c56a29d64869c96e800e240293f2
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-07-14 22:54:44 +0530

    initial files

---
+++ areacalc.py
@@ -0,0 +1,6 @@
+'''The length 'L' and breadth 'B' of a rectangle are passed as input. The program must calculate
+the perimeter of the rectangle and print the output. Both L and B are positive integers.
+Output displays the calculated perimeter'''
+l=int(input("enter the length of the rectangle"))
+b=int(input("enter the breadth of the rectangle"))
+print(f"The area = {2*(l+b)} cm^2")
